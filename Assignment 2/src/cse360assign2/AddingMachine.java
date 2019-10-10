40672e6d1da62ff486844bc69400900acca691e7
Vamshi Avvari <vamshi.avvari@asu.edu>
2019-10-09 21:26:56 -0700

    Final version of AddingMachine

--- Assignment 2/src/cse360assign2/AddingMachine.java
+++ Assignment 2/src/cse360assign2/AddingMachine.java
@@ -6,29 +6,33 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
-	private String log = "0 "; 
+	private String log = "0"; // String variable for recording history
 	
 	public AddingMachine () { // default constructor for adding machine
 		total = 0;  // not needed - included for clarity
+		log = "0";  // included for clarity
 	}
 	
 	public int getTotal () {	// returns current total
-		return 0;
+		return total; 
 	}
 	
 	public void add (int value) {	// add a value to the total
-		
+		total = total + value;
+		log = log + " + " + value;  // updates history when method is called
 	}
 	
 	public void subtract (int value) { // subtract value parameter from total
-		
+		total = total - value;
+		log = log + " - " + value;     // updates history when method is called
 	}
 		
 	public String toString () { 	// returns a record of transactions
-		return "";
+		return log;
 	}
 
 	public void clear() {	// clears history of adding machine
-	
+		log = "0";			// reset total and log to initial state
+		total = 0;
 	}
 }
