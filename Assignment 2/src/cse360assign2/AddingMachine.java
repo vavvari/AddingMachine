7c8ecfbfb9c147b47a0c4dc340fefcd23891c339
Vamshi Avvari <vamshi.avvari@asu.edu>
2019-10-09 20:23:14 -0700

    Initial Adding Machine class

---
+++ Assignment 2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,34 @@
+// Name: Vamshi Avvari
+// Assignment 2
+// Class ID: 85141
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	private String log = "0 "; 
+	
+	public AddingMachine () { // default constructor for adding machine
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {	// returns current total
+		return 0;
+	}
+	
+	public void add (int value) {	// add a value to the total
+		
+	}
+	
+	public void subtract (int value) { // subtract value parameter from total
+		
+	}
+		
+	public String toString () { 	// returns a record of transactions
+		return "";
+	}
+
+	public void clear() {	// clears history of adding machine
+	
+	}
+}
